74261dae81c2b45ebac8483b41b1450f5ff5b4b7
Luiz Pereira <luizfper@gmail.com>
2019-02-04 14:41:00 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,48 @@
-# Write your code here.
+def dictionary(word)
+  dic={hello: 'hi',
+  too: '2',
+  to: '2',
+  two: '2',
+  for:'4',
+  four:'4',
+  be:'b',
+  you:'u',
+  at:'@',
+  and:'&'}
+  dic[word.to_sym]
+end# Write your code here.
+
+def word_substituter(long)
+  long_array=long.split
+  fin_array=[]
+  long_array.each do |word|
+    if dictionary(word.downcase)!=nil
+      fin_array<<dictionary(word.downcase)
+    else
+      fin_array<<word
+    end
+  end
+  fin_array.join(" ")
+end
+
+def bulk_tweet_shortener(tweets)
+  fin_array=[]
+  tweets.each do |tweet|
+    #if tweet.length >140
+      puts word_substituter(tweet)
+    #else
+      #puts tweet.to_s
+    #end
+  end
+end
+
+def selective_tweet_shortener(tweets)
+  fin_array=[]
+  tweets.each do |tweet|
+    if tweet.length >140
+      puts word_substituter(tweet)
+    else
+      puts tweet.to_s
+    end
+  end
+end
